ea1b2a7570c33f32eb92bcde46c21419cf5597f0
jaewon0123 <169113696+jaewon0123@users.noreply.github.com>
2024-07-25 14:08:15 +0900

    async await 사용해서 insert select

---
+++ mysql-front/src/component/UserForm.js
@@ -0,0 +1,27 @@
+import React, {useState} from "react";
+
+const UserForm = ({addUser}) => {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+
+    const handlSubmit = (e) => {
+        e.preventDefault(); //잠시 제출 방지
+        addUser({name, email});
+    }
+    return (
+        <div>
+            <form onSubmit={handlSubmit}>
+                <div>
+                    <label>이름 : </label>
+                    <input type="text" value={name} onChange={(e) => setName(e.target.value)} required/>
+                </div>
+                <div>
+                    <label>이메일 : </label>
+                    <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} required/>
+                </div>
+                <button>유저 추가하기</button>
+            </form>
+        </div>
+    )
+}
+export default UserForm;
