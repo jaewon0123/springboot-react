5290dcfe7832327b237cba02b401ff2216a006e7
jaewon0123 <169113696+jaewon0123@users.noreply.github.com>
2024-08-21 10:50:51 +0900

    Modal 이용해서 치킨 메뉴 입력받기, 치킨 리스트 보여주기

---
+++ chicken-store/src/component/ChikenForm.js
@@ -0,0 +1,46 @@
+import React, {useState, useEffect} from "react";
+import axios from "axios";
+import '../css/ChickenForm.css';
+
+const ChickenForm = () => {
+    const [chickenName, setChickenName] = useState('');
+    const [description, setDescription] = useState('');
+    const [price, setPrice] = useState('');
+
+    const 전달데이터 = {
+        chickenName,
+        description,
+        price
+    }
+    // 스프링부트 연결 후 전달
+    const 제출버튼 = () => {
+        axios.post('http://localhost:9090/api/chicken',전달데이터)
+        .then(response => {
+            // 데이터 무사히 전달했을 경우
+            alert('등록이 완료되었습니다.');
+        })
+        .catch(e => {
+            alert('메뉴 등록에 실패하였습니다.')
+            console.error('error : ' , e)
+        });
+    }
+    return (
+        <div className="chickenform-container">
+            <label>
+                메뉴 이름 :
+                <input type="text" onChange={e => setChickenName(e.target.value)} value={chickenName}/>
+            </label>
+            <label>
+                메뉴 설명 :
+                <textarea onChange={e => setDescription(e.target.value)} value={description}/>
+            </label>
+            <label>
+                가격 :
+                <input type="number" onChange={e => setPrice(e.target.value)} value={price}/>
+            </label>
+            <button onClick={제출버튼}>등록하기</button>
+        </div>
+    )
+}
+
+export default ChickenForm;
