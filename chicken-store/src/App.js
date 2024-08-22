b4b8f54d6e831413df3bb06c3b58f5484c400fc7
jaewon0123 <169113696+jaewon0123@users.noreply.github.com>
2024-08-22 11:46:02 +0900

    치킨 메뉴 업데이트, 삭제

--- chicken-store/src/App.js
+++ chicken-store/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import ChickenList from './component/ChickenList';
 import ChickenDetail from './component/ChickenDetail';
+import MainRouter from './MainRouter';
 
 
 function App () {
@@ -9,7 +10,8 @@ function App () {
     return (
         <Router>
             <Routes>
-                <Route path='/' element={<ChickenList/>}/>
+                <Route path='/' element={<MainRouter/>}/>
+                {/* Routes 안에는 Route로 설정된 태그만 들어올 수 있음 <MainRouter/>*/}
                 <Route path='/chicken-detail/:id' element={<ChickenDetail/>}/>
             </Routes>
         </Router>
